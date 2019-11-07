b0f22ed8822789d92616d537e9816d03cb9fb829
Семен Згурский <saymon221090@gmail.com>
2019-11-07 18:46:13 +0300

    Добавляет функционирующую фильтрацию. Возможно не самый оптимальный код

--- js/filter.js
+++ js/filter.js
@@ -4,20 +4,68 @@
 
   var PINS_QUANTITY = 5;
   var container = document.querySelector('.map__filters');
+  var controlsList = container.querySelectorAll('.map__filter');
   var housingType = container.querySelector('#housing-type');
+  var housingRooms = container.querySelector('#housing-rooms');
+  var housingGuests = container.querySelector('#housing-guests');
+  var housingPrice = container.querySelector('#housing-price');
+  var checkboxesContainer = document.querySelector('#housing-features');
+  var checkboxes = checkboxesContainer.querySelectorAll('.map__checkbox');
+  var checkboxesArr = Array.from(checkboxes);
 
-  var getProcessedData = function (loadedData) {
-
-    var sameHousingType = loadedData.filter(function (it) {
-      return it.offer.type === housingType.value || housingType.value === 'any';
-    });
-
-    return sameHousingType.slice(0, PINS_QUANTITY);
+  var compareFeatures = function (dataFeatures, selectedFeatures) {
+    for (var i = 0; i < selectedFeatures.length; i++) {
+      if (dataFeatures.indexOf(selectedFeatures[i]) < 0) {
+        return false;
+      }
+    }
+    return true;
   };
 
-  housingType.addEventListener('change', function () {
-    window.pin.removeActiveItems();
-    window.pin.getRenderedItems(window.pin.originalData);
+  var getProcessedData = function (loadedData) {
+    var selectedFeatures = checkboxesArr.filter(function (it) {
+      return it.checked === true;
+    })
+      .map(function (it) {
+        return it.value;
+      });
+
+    var processedData = loadedData.filter(function (it) {
+      return it.offer.type === housingType.value || housingType.value === 'any';
+    })
+      .filter(function (it) {
+        return it.offer.rooms === Number(housingRooms.value) || housingRooms.value === 'any';
+      })
+      .filter(function (it) {
+        return it.offer.guests === Number(housingGuests.value) || housingGuests.value === 'any';
+      })
+      .filter(function (it) {
+        return compareFeatures(it.offer.features, selectedFeatures);
+      })
+      .filter(function (it) {
+        if (housingPrice.value === 'middle') {
+          return it.offer.price > 10000 && it.offer.price < 50000;
+        } else if (housingPrice.value === 'low') {
+          return it.offer.price <= 10000;
+        } else if (housingPrice.value === 'high') {
+          return it.offer.price >= 50000;
+        }
+        return true;
+      });
+
+
+    return processedData.slice(0, PINS_QUANTITY);
+  };
+
+  var onValueChange = function (control) {
+    control.addEventListener('change', function () {
+      window.pin.removeActiveItems();
+      window.pin.getRenderedItems(window.pin.originalData);
+    });
+  };
+
+  controlsList.forEach(function (it) {
+    onValueChange(it);
   });
 
   window.filter = {
